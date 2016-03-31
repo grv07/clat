a1ef9767e3c3833be6b35818c0c7da791ae2f616
Ubuntu <ubuntu@ip-172-31-55-28.ec2.internal>
2016-03-31 07:42:49 +0000

    static file setup done with gunicorn

--- CLAT/settings.py
+++ CLAT/settings.py
@@ -246,11 +246,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/var/www/server_static'
+STATIC_ROOT = '/var/www/server_static/'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static/"),
-    '/var/www/static/',
+    #'/var/www/server_static/',
 )
 
 MEDIA_ROOT = '/lms/media/'
