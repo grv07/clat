773482d2163598a62eb268fc31cda1c0952c8f7a
Gaurav Tygai <grvtyagi22@gmail.com>
2016-05-09 19:01:50 +0530

    add QnA home url

--- qna_api/user_manager.py
+++ qna_api/user_manager.py
@@ -1,11 +1,11 @@
 import requests
+from CLAT.settings import QNA_PATH
 
-ORIGIN_URL = 'http://52.77.85.150/api/'
 #ORIGIN_URL = 'http://localhost:8000/'
 
 def register_user(username, email, test_key = 'c3vsg3jcp7'):
     url = 'user/data/'
-    r = requests.post(ORIGIN_URL+url, data = {'username': username, 'email': email, 'test_key':test_key})
+    r = requests.post(QNA_PATH+url, data = {'username': username, 'email': email, 'test_key':test_key})
     return r.text
 
     
